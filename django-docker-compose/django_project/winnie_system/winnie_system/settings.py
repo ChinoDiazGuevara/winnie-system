bea30157246cbfd2054630432fcf47dfe61a8e89
Matias Diaz Guevara <mguevara@dthinkdata.com>
2022-10-24 01:19:22 -0300

    0120 24/10/22

--- django-docker-compose/django_project/winnie_system/winnie_system/settings.py
+++ django-docker-compose/django_project/winnie_system/winnie_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t0ueyl%m@sv*3s73!gzk6xxlm4)*qc&is%)p7k(3#4z6sxfc31'
+#SECRET_KEY = 'django-insecure-t0ueyl%m@sv*3s73!gzk6xxlm4)*qc&is%)p7k(3#4z6sxfc31'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
